d198e08edb93796c564d3b2d5f11df841c865330
Christian Rojas <68656093+ChrissRojas@users.noreply.github.com>
2023-01-25 17:27:22 +0000

    Add Script

--- script.js
+++ script.js
@@ -5,14 +5,34 @@ let player_score_object = document.querySelector('#player-score');
 let comp_score_object = document.querySelector('#comp-score');
 let player_choices = document.querySelectorAll('.player-choice');
 let current_choice;
+let comp_current_choice;
 player_score_object.textContent = `Your-Score: ${player_score}`;
 comp_score_object.textContent= `Comp-Score: ${comp_score}`;
 
 
 
 function playGame(e){
-    console.log(this)
+    
+
+    if (current_choice){
+        current_choice.classList.remove('selected');
+        comp_current_choice.classList.remove('selected');
+    }
+    let comp_choice = getComputerChoice();
+    let comp_element = document.querySelector(`#comp-${comp_choice}`)
+    console.log
+    current_choice = this;
+    comp_current_choice = comp_element;
+    current_choice.classList.add('selected');
+    comp_current_choice.classList.add('selected');
+
+    let current_choice_id = current_choice.id.slice(7);
+    playRound(current_choice_id,comp_choice)
+    
 }
+
+
+
 function getRandomInt(min,max) {
     return Math.floor(Math.random() * (max - min)) + min; 
 }
@@ -26,26 +46,40 @@ function playRound(playerSelection,compSelection){
     playerSelection = playerSelection.toLowerCase();
     if (playerSelection ===  compSelection) {
         console.log('draw');
+
     }else if (playerSelection == 'rock') {
         if (compSelection == 'paper'){
             console.log('You lose, paper beats rock!');
+            comp_score++;
+            comp_score_object.textContent= `Comp-Score: ${comp_score}`;
         }else{
             console.log('You win, rock beats scissors');
+            player_score++;
+            player_score_object.textContent = `Your-Score: ${player_score}`;
         }
     }else if(playerSelection =='paper'){
         if(compSelection =='scissors'){
             console.log('You lose, scissors beats paper');
+            comp_score++;
+            comp_score_object.textContent= `Comp-Score: ${comp_score}`;
         }else{
             console.log('You win, paper beats rock');
+            player_score++;
+            player_score_object.textContent = `Your-Score: ${player_score}`;
+            
         }
     }else if(playerSelection =='scissors'){
         if (compSelection == 'rock'){
             console.log('You lose, rock beats scissors');
+            comp_score++;
+            comp_score_object.textContent= `Comp-Score: ${comp_score}`;
         }else{
-            console.log('You win,scissoes beats paper');
+            console.log('You win,scissors beats paper');
+            player_score++;
+            player_score_object.textContent = `Your-Score: ${player_score}`;
         }
     }
 }
 
 
-player_choices.forEach(k => k.addEventListener('click',play))
+player_choices.forEach(k => k.addEventListener('click',playGame))
